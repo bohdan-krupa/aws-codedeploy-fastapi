15407651eb56a1b63fb978f615551b39d1684cb7
Bohdan <bodya.krupa@gmail.com>
2023-01-28 02:14:06 +0200

    fix actions

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_docs():
-    response = requests.get("http://localhost:8000/docs")
+    response = requests.get("http://localhost/docs")
     assert response.status_code == 200
